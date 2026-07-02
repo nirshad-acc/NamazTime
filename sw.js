6557562d274ebb6197e1b1c38e72ed88e19c9ae6
nirshad-acc <nirshad.acc@gmail.com>
2026-07-02 12:56:59 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'namaz-times-v29';
+const CACHE_NAME = 'namaz-times-v30-02-Jul-2026-1256';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -16,11 +16,15 @@ self.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
-// 2. Activate Event: Cleans up old caches if we update the app in the future
+// 2. Install Event: Saves the core app shell files into phone memory
+
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
+    (async () => {
+      // Delete old caches
+      const cacheNames = await caches.keys();
+
+      await Promise.all(
         cacheNames.map((cache) => {
           if (cache !== CACHE_NAME) {
             console.log('Clearing old cache structure...', cache);
@@ -28,21 +32,26 @@ self.addEventListener('activate', (event) => {
           }
         })
       );
-    })
-  );
-  return self.clients.claim();
-});
 
-self.addEventListener("activate", () => {
-  self.clients.matchAll().then((clients) => {
-    clients.forEach((client) => {
-      client.postMessage({
-        type: "SW_UPDATED"
+      // Take control of all open clients
+      await self.clients.claim();
+
+      // Notify all open pages that a new SW is active
+      const clients = await self.clients.matchAll({
+        type: "window",
+        includeUncontrolled: true
       });
-    });
-  });
-});
 
+      clients.forEach(client => {
+        client.postMessage({
+          type: "SW_UPDATED"
+        });
+      });
+
+      console.log("Service Worker activated.");
+    })()
+  );
+});
 // 3. Fetch Event: Intercepts requests to serve cached files instantly when offline
 self.addEventListener('fetch', (event) => {
   // We only want to handle local files (not your live Google Apps Script API calls)
