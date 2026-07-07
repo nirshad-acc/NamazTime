ce8219eaed5543700caa1761080bb01022a9f8f5
nirshad-acc <nirshad.acc@gmail.com>
2026-07-07 15:44:38 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'namaz-times-v44-07-Jul-2026-1536_removing_2ndSettingsIcon-UX';
+const CACHE_NAME = 'namaz-times-v45-07-Jul-2026-1544_removing_2ndSettingsIcon-UX';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
