4157c70b5831181ec75d4470b9e9633b25d4c0a2
nirshad-acc <nirshad.acc@gmail.com>
2026-07-08 15:57:27 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'namaz-times-v49-08-Jul-2026-1550_activating_offsetBox';
+const CACHE_NAME = 'namaz-times-v50-08-Jul-2026-1600_activating_offsetBox';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
